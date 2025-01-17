184cf348e6e267d6983f7553a171f8001c4a15c6
Aditya Mishra <108024126+adityarajmishraji@users.noreply.github.com>
2025-01-17 05:44:22 +0000

    Arthmetic Operation

--- Python49/day1/q2.py
+++ Python49/day1/q2.py
@@ -6,4 +6,4 @@ sim_int = (pri*rate*num_year)//100
 
 # here I add "//" insted of using "/" because we don't need any floating value as a result
 
-print("Simple interest is :",sim_int)
+print("Simple interest is :", sim_int)
