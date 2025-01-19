0af155d1edb6f1394165b047416ecaf0742c80f3
Aditya Mishra <108024126+adityarajmishraji@users.noreply.github.com>
2025-01-19 05:45:52 +0000

    Simple Arthmatic Operations

--- Java_Practice/math.java
+++ Java_Practice/math.java
@@ -1,21 +1,18 @@
-class math{
-    int a =12;
-    int b = 5;
-    int add = a + b;
-    int sub = a - b;
-    int multi = a * b;
-    int div = a / b;
-    int remainder = a % b;
+class MathOperations {
 
-    void displayResults() {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 5;
+        int add = a + b;
+        int sub = a - b;
+        int multi = a * b;
+        int divide = a / b;
+        int remainder = a % b;
+
         System.out.println("Addition of " + a + " and " + b + " is: " + add);
         System.out.println("Subtraction of " + a + " and " + b + " is: " + sub);
         System.out.println("Multiplication of " + a + " and " + b + " is: " + multi);
         System.out.println("Division of " + a + " and " + b + " is: " + divide);
         System.out.println("Remainder of " + a + " and " + b + " is: " + remainder);
-    }
-    public static void main(String[] args) {
-        math math = new math();
-        math.displayResults();
     }
 }
