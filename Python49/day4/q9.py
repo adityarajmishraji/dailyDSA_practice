3ff3dcfe11e3cc0327e08d65788a570a53fc0f02
Aditya Mishra <108024126+adityarajmishraji@users.noreply.github.com>
2025-01-19 03:43:05 +0000

    Finding the Maximum of Three Numbers

---
+++ Python49/day4/q9.py
@@ -0,0 +1,12 @@
+n1 = int(input("Enter First Number :"))
+n2 = int(input("Enter Second Number :"))
+n3 = int(input("Enter Third Number :"))
+
+if n1 > n2 and n1 > n3:
+    print("First Number is Maximum")
+elif n2 > n1 and n2 > n3:
+    print("Second Number is Maximum")
+elif n3 > n1 and n3 > n2:
+    print("Third Number is Minimum")
+else:
+    print("some are same or all number is zero")
