5494c8d2476fab0b778b4a5e1dfb4ccf20f88f9e
Aditya Mishra <108024126+adityarajmishraji@users.noreply.github.com>
2025-01-19 04:16:33 +0000

    printing n3 write as maxii

--- Python49/day4/q9.py
+++ Python49/day4/q9.py
@@ -7,6 +7,6 @@ if n1 > n2 and n1 > n3:
 elif n2 > n1 and n2 > n3:
     print("Second Number is Maximum")
 elif n3 > n1 and n3 > n2:
-    print("Third Number is Minimum")
+    print("Third Number is Maximum")
 else:
     print("some are same or all number is zero")
