34e094c262712cdd37cd1e087de0b55fd8cec713
Aditya Mishra <108024126+adityarajmishraji@users.noreply.github.com>
2025-01-19 03:31:34 +0000

    Real number

---
+++ Python49/day4/q6.py
@@ -0,0 +1,8 @@
+number = int(input("Enter Any Real Number :"))
+print("Real no. is  : - to  ") # Alter+8712 = belongs to sign and alter+8734 is infinite sigh 
+if number > 0:
+    print("Number is Positive")
+elif number < 0:
+    print("Number is Negetive")
+else:
+    print("Number is Zero")
