4853b5294d20d14546d0d75dbe6e223af35d8ec0
Aditya Mishra <108024126+adityarajmishraji@users.noreply.github.com>
2025-01-21 07:16:34 +0000

    Find the average of five number.

---
+++ Python49/day6/q24.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter Your  First Number :"))
+num2 = int(input("Enter  Your Second Number :"))
+num3 = int(input("Enter Your  Third Number :"))
+num4 = int(input("Enter  Your Fourth Number :"))
+num5 = int(input("Enter Your  Fifth Number :"))
+
+sum = num1 + num2 + num3 + num4 + num5
+
+avg = sum / 5
+
+print("average value of five number is ",avg)
