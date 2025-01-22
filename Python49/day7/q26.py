44638b28c626f0e61c733730d6faf6b9c2202c81
Aditya Mishra <108024126+adityarajmishraji@users.noreply.github.com>
2025-01-22 08:35:19 +0000

    Average of odd number.

---
+++ Python49/day7/q26.py
@@ -0,0 +1,15 @@
+number = int(input("Enter any number : "))
+
+total = 0
+count = 0
+
+for i in range(1, number+1):
+        if(i % 2 != 0):
+                print(i)
+                total = total + i
+                count = count + 1
+                
+avg = total / count
+
+print("Sum of the First Natural number is",total)
+print("Average of Odd Number is :",avg)
