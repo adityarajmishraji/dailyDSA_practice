fa97423e7fb53f880d35912c87ad8c1af8e7f6ca
Aditya Mishra <108024126+adityarajmishraji@users.noreply.github.com>
2025-01-23 14:24:05 +0000

    your response plz

---
+++ Python49/day8/q30.py
@@ -0,0 +1,9 @@
+number1 = int(input("Enter any number :"))
+number2 = int(input("Enter any number :"))
+
+if number1 / number2:
+	print(number1,"Divisible by",number2)
+else:
+	print(number1,"is not Divisible by",number2)
+	
+    # Waiting for some on response...
