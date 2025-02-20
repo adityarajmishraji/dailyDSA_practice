9c4fb66541c051a931a580973975739e94563bb7
Aditya Mishra <108024126+adityarajmishraji@users.noreply.github.com>
2025-02-20 03:49:35 +0000

    Real Number.

--- Python49/day4/q6.py
+++ Python49/day4/q6.py
@@ -1,5 +1,5 @@
 number = int(input("Enter Any Real Number :"))
-print("Real no. is  : - to  ") # Alter+8712 = belongs to sign and alter+8734 is infinite sigh 
+print("Real no. is {belongs : -infinite to  ") # Alter+8712 = belongs to sign and alter+8734 is infinite sigh 
 if number > 0:
     print("Number is Positive")
 elif number < 0:
