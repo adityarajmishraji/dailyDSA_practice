d393638555452f028eb513cac813096afd475e52
Aditya Mishra <108024126+adityarajmishraji@users.noreply.github.com>
2025-02-20 03:39:22 +0000

    Check and show the product name and net amount(after discount).

--- Python49/day3/q4.py
+++ Python49/day3/q4.py
@@ -9,20 +9,17 @@ des_amt = 0
 if pro_id == 1 and ord_amt > 1000:
     des_amt = ord_amt * 10 / 100
     net_amt = ord_amt - des_amt
-    print("Product Name is Transistors.")
-    print("Total Amount =",net_amt)
+    print("Product Name is Transistors. \n Total Amount =",net_amt)
 
 if pro_id == 2 and ord_amt > 100:
     des_amt = ord_amt * 5 / 100
     net_amt = ord_amt - des_amt
-    print("Product Name is Resisters.")
-    print("Total Amount =",net_amt)
+    print("Product Name is Resisters. \n Total Amount =",net_amt)
 
 if pro_id == 3 and ord_amt > 500:
     des_amt = ord_amt * 10 / 100
     net_amt = ord_amt - des_amt
-    print("Product Name is Capacitors.")
-    print("Total Amount =",net_amt)
+    print("Product Name is Capacitors. \n Total Amount =",net_amt)
 
 if pro_id > 3:
-    print("Enter Proper Product id!!!")
+    print(" Enter Proper Product id!!! \n Product_ID should not be more than 3!!!")
