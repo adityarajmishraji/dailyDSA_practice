6c3d0399472066b84fce247c92d9591f18d7eca1
Aditya Mishra <108024126+adityarajmishraji@users.noreply.github.com>
2025-02-21 18:12:05 +0000

    Samjhoo toh Janu .But The simple catch is that give me real world use case.

--- Python49/day6/q14.py
+++ Python49/day6/q14.py
@@ -8,3 +8,11 @@ for i in range(1,num + 1):
         res -= i ** 2
 
 print("Total :", res)
+
+# i	Even/Odd	Operation	Result (res)
+# 1	Odd	        res - (1^2)	1 - 1 = 0
+# 2	Even	     + (2^2)	0 + 4 = 4
+# 3	Odd	res - (3^2)	4 - 9 = -5
+# 4	Even	res + (4^2)	-5 + 16 = 11
+# 5	Odd	res - (5^2)	11 - 25 = -14
+
